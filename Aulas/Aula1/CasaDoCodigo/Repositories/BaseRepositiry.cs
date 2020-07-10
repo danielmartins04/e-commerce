1238b3875053fa00af126b12e6686169e223ae18
daniel martins <freitasdaniel39@gmail.com>
2020-07-10 20:02:11 -0300

    baserepository

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/BaseRepositiry.cs
@@ -0,0 +1,22 @@
+﻿using CasaDoCodigo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class BaseRepositiry<T> where T : BaseModel
+    {
+        protected readonly ApplicationContext contexto;
+        protected readonly DbSet<T> dbSet;
+
+        public BaseRepositiry(ApplicationContext contexto)
+        {
+            this.contexto = contexto;
+            dbSet = contexto.Set<T>();
+
+        }
+    }
+}
