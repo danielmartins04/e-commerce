eaa164649e0c8487a982cba2b919e8d5e203a7f9
daniel martins <freitasdaniel39@gmail.com>
2020-07-19 10:10:54 -0300

    injecting the registration in the view and redirecting if necessary

--- Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
+++ Aulas/Aula1/CasaDoCodigo/Controllers/PedidoController.cs
@@ -42,7 +42,14 @@ namespace CasaDoCodigo.Controllers
 
         public IActionResult Cadastro()
         {
-            return View();
+            var pedido = pedidoRepository.GetPedido();
+
+            if (pedido == null)
+            {
+                return RedirectToAction("Carrossel");
+            }
+
+            return View(pedido.Cadastro);
         }
 
         public IActionResult Resumo()
