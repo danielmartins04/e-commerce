ac6dde706139ae31cb128e6d69e7b77816964f37
daniel martins <freitasdaniel39@gmail.com>
2020-07-19 20:20:41 -0300

    validating form data on the server

--- Aulas/Aula1/CasaDoCodigo/Repositories/CadastroRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/CadastroRepository.cs
@@ -9,13 +9,18 @@ namespace CasaDoCodigo.Repositories
 
     public interface ICadastroRepository
     {
-
+        Cadastro Update(int cadastroId, Cadastro novoCadastro);
     }
 
     public class CadastroRepository : BaseRepositiry<Cadastro>, ICadastroRepository
     {
         public CadastroRepository(ApplicationContext contexto) : base(contexto)
         {
+        }
+
+        public Cadastro Update(int cadastroId, Cadastro novoCadastro)
+        {
+            throw new NotImplementedException();
         }
     }
 }
